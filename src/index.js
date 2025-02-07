9368963d880edf1cdcd447c549d787423058fffb
Atif Pathan <atifkhan.p1@gmail.com>
2025-02-06 18:15:07 -0700

    create renderGame module to handle all rendering, including initialization of the grids

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './styles.css';
+import { initBoard } from './modules/renderGame,js';
+// import Player from './modules/Player.js';
+// import Ship from './modules/Ship.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Initialize the boards
+  initBoard('player-grid', false); // Player board (non-clickable)
+  initBoard('enemy-grid', true); // Enemy board (clickable)
+
+  //   // Create players
+  //   const player = new Player('real');
+  //   const computer = new Player('computer');
+
+  //   // Populate boards with predetermined placements, for now.
+  //   player.gameBoard.placeShip(new Ship(3), 1, 1);
+  //   player.gameBoard.placeShip(new Ship(4), 5, 2);
+  //   computer.gameBoard.placeShip(new Ship(2), 0, 0);
+  //   computer.gameBoard.placeShip(new Ship(5), 7, 3);
+
+  //   // Render the boards
+  //   renderBoard(player.gameBoard, 'player-grid');
+  //   renderBoard(computer.gameBoard, 'enemy-grid');
+});
